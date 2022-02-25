65cede74ae18a8c7e27037346fdfaa1046c01394
Claudia Oprescu <claudia@moo.com>
2022-02-25 18:12:25 +0200

    List All Vets in Index Vet Page. Closes #14

--- pet-clinic-web/src/main/java/com/clinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/clinic/bootstrap/DataLoader.java
@@ -45,6 +45,7 @@ public class DataLoader implements CommandLineRunner {
 //        Speciality savedDentistry = specialtyService.save(dentistry);
 
         Owner owner1 = new Owner();
+        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 //        owner1.setAddress("123 Brickerel");
@@ -61,6 +62,7 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
+        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
 //        owner2.setAddress("123 Brickerel");
@@ -86,6 +88,7 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
+        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 //        vet1.getSpecialities().add(savedRadiology);
@@ -93,6 +96,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
+        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 //        vet2.getSpecialities().add(savedSurgery);
